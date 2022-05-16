ece0f2a6daa19142e6f244902a1a7dbccb5096e0
Victor Rodriguez <victorm.rodriguez@alumnos.udg.mx>
2022-05-16 17:02:19 -0500

    Index de Gerente, y parte de Mesero y Cocinero

---
+++ main.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!--
+    <h1>KOMAN'DAH</h1>
+    <table>
+        <tr>
+            <th><h3>Empleados</h3></th>
+            <th><h3>Productos</h3></th>
+            <th><h3>Comandas</h3></th>
+        </tr>
+        <tr>
+            <td><a href="alta_empleado.php">ingresa empleado</a></td>
+            <td><a href="alta_producto.php">alta producto</a></td>
+            <td><a><a href="orden.php">levantar orden</a></a></td>
+        </tr>
+    </table>
+    -->
+
+    <div id="header">
+        <ul class="nav">
+            <li>Inicio</li>
+
+            <li>Empleados
+                <ul>
+                    <li>Alta</li>
+                    <li>Baja</li>
+                    <li>Modificacion</li>
+                    <li>Mostrar</li>
+                </ul>
+            </li>
+
+            <li>Productos
+                <ul>
+                    <li>Alta</li>
+                    <li>Baja</li>
+                    <li>Modificacion</li>
+                    <li>Mostrar</li>
+                </ul>
+            </li>
+
+            <li>Comandas</li>
+
+            <li>Mesas</li>
+        </ul>
+    </div>
+</body>
+</html>
