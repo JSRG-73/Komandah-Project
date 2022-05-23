adf1cd1c7b13215ae40fdb71b4b797af440ce1e3
Victor Rodriguez <victorm.rodriguez@alumnos.udg.mx>
2022-05-22 20:19:02 -0500

    Para solucinar el punto y coma xd

--- Gerente/Empleados/display.php
+++ Gerente/Empleados/display.php
@@ -17,43 +17,45 @@
 <body>
     <div class="container">
         <button class="btn btn-success my-5"> <a href="empleado.php" class="text-light">Agregar</a> </button>
-        <button class="btn btn-primary my-5"> <a href="../index.php" class="text-light">Volver</a> </button>
-        <button class="btn btn-warning my-5"><a href="#" class="text-light" >Modificar</a></button>
-        <button class="btn btn-danger my-5"><a href="#" class="text-light">Eliminar</a></button>
+        <button class="btn btn-primary my-5"> <a href="../index.php" class="text-light">Inicio</a> </button>
         <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">ID Empleado</th>
-      <th scope="col">Nombre</th>
-      <th scope="col">Usuario</th>
-      <th scope="col">Rol</th>
-    </tr>
-  </thead>
-  <tbody>
-      <?php
-      $con = conectar();
-      $sql = "SELECT id, nombre, usuario, rol FROM usuarios where activo = 1"; 
-      $resultado = mysqli_query($con,$sql);
-      if($resultado){
-          while($row=mysqli_fetch_assoc($resultado)){ 
-              $id = $row['id'];
-              $nombre = $row['nombre'];
-              $usuario = $row['usuario'];
-              $rol = $row['rol'];
-              echo '<tr>;
-              <td>'.$id.'</td> 
-              <td>'.$nombre.'</td> 
-              <td>'.$usuario.'</td> 
-              <td>'.$rol.'</td> 
+          <thead>
+            <tr>
+              <th scope="col">ID Empleado</th>
+              <th scope="col">Nombre</th>
+              <th scope="col">Usuario</th>
+              <th scope="col">Rol</th>
+            </tr>
+          </thead>
+          <tbody>
+              <?php
+              $con = conectar();
+              $sql = "SELECT id, nombre, usuario, rol FROM usuarios where activo = 1"; 
+              $resultado = mysqli_query($con,$sql);
+              if($resultado){
+                  while($row=mysqli_fetch_assoc($resultado)){ 
+                      $id = $row['id'];
+                      $nombre = $row['nombre'];
+                      $usuario = $row['usuario'];
+                      $rol = $row['rol'];
+                      echo '<tr>;
+                      <td>'.$id.'</td> 
+                      <td>'.$nombre.'</td> 
+                      <td>'.$usuario.'</td> 
+                      <td>'.$rol.'</td> 
+                      <td>
+                      <button class="btn btn-warning"><a href="delete.php?id='.$id.'" class="text-light">(Pending)</a></button>
+                      <button class="btn btn-danger"onclick=eliminarEmpleado('.$id.');>Eliminar</a></button>
+                      </td>
               </tr>';
-            }
-      }
-      ?>
-  </tbody>
-</table>
-    </div>
-    <div class="mensaje" id="mensajeEliminar">
-        Mensaje
-    </div>
+                    }
+              }
+              ?>
+          </tbody>
+        </table>
+      </div>
+      <div class="mensaje" id="mensajeEliminar">
+          Mensaje
+      </div>
 </body>
 </html>
