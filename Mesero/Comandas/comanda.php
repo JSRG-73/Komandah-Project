87b4ac40cd4e719e64a47a39b994301a3d978bb9
JSRG-73 <jorge.99rg@gmail.com>
2022-05-22 20:40:28 -0500

    avances en la creacion de comandas

--- Mesero/Comandas/comanda.php
+++ Mesero/Comandas/comanda.php
@@ -11,15 +11,49 @@
     <form method="post" name="form1">
     <div class="form-group my-5" >
     
-        <label >Nombre</label>
+        <label >Mesa</label>
+        <select id="cars" name="carlist" form="carform">
+
+</select>
+        <?php
+        require "../../Funciones/Conectar.php";
+      $con = conectar();
+      $sql = "SELECT COUNT(*) AS contador FROM mesas;"; 
+      $resultado = $con -> query($sql);
+
+      $row=mysqli_fetch_assoc($resultado);
+        $contador = $row['contador'];
+            echo $contador;
+            foreach(range(1,$contador) as $x){
+
+                echo $x;
+
+            }
+      ?>
+
+
         <input type="text" class="form-control" name="nombre" autocomplete="off" placeholder="Nombre del producto" id="inputnombre">
-        <label >Precio</label>
+        <label >Producto</label>
         <input type="number" class="form-control" name="precio" autocomplete="off" id="inputprecio">
+        <label >Cantidad</label>
+        <input type="text" class="form-control" name="nombre" autocomplete="off" placeholder="Nombre del producto" id="inputnombre">
+
+        <?php
+            echo '<h1>qwerty</h1>';
+            echo '<td>';
+        ?>
+
+        <label >Notas</label>
+        <input type="number" class="form-control" name="precio" autocomplete="off" id="inputprecio">
+
+        
+
     </div>
+
+
     <input type="submit" class="btn btn-primary name" name="register" onclick="verificarLlenos(); return false;">
     <!-- La función verificarLlenos también envialos datos a la BD -->
 
-    
     <button class="btn btn-primary my-5"> <a href="../index.php" class="text-light">Volver</a> </button>
 
     </form>
---
+++ Mesero/Comandas/scripts.js
@@ -0,0 +1,19 @@
+function ValidarDatos(){
+
+        let datosllenos = document.getElementById("inputnombre").value !== "" ? true : false;
+        datosllenos = datosllenos && document.getElementById("inputprecio").value !== "" ? true : false;
+
+        return datosllenos;
+}
+
+function verificarLlenos(){
+    if (ValidarDatos()){
+        document.form1.method = 'post'
+        document.form1.action = 'querys.php'
+        document.form1.enctype= "multipart/form-data"
+        document.form1.submit();
+    }
+    else{
+        alert("Faltan datos por ingresar");
+    }
+}
