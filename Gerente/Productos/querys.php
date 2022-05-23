0e4733abd7acc8e69cd23a04f158cf13fc8eda60
Victor Rodriguez <victorm.rodriguez@alumnos.udg.mx>
2022-05-22 22:46:19 -0500

    Agregar productos y visualizarlos jalando

--- Gerente/Productos/querys.php
+++ Gerente/Productos/querys.php
@@ -4,5 +4,12 @@
     $nombre = $_REQUEST['nombre'];
     $precio = $_REQUEST['precio'];
     $sql = "INSERT INTO productos(nombre,precio) VALUES ('$nombre','$precio');";
+    if(isset($_REQUEST['descripcion'])){
+      $desc = $_REQUEST['descripcion'];
+      if($desc != '' && $desc != null){
+        $sql = "INSERT INTO productos(nombre,precio,descripcion) VALUES ('$nombre','$precio','$desc');";
+      }
+  }
     $respuesta = $conexion -> query($sql);
+    header('location: ./display.php');
 ?>
